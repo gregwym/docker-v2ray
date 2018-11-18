1fbc6f1189ac1b1c5e243191f0e629a00e10df7c
Greg Wang <gregwym@users.noreply.github.com>
2018-11-18 23:15:42 +0800

    Add setup script

---
+++ setup-centos7.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add repo
+yum install epel-release
+
+# Install docker
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+systemctl start docker
+systemctl enable docker
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
