de6eaa2dd3f381fb6a7327ba319d6785724f1cc4
Greg Wang <gregwym@users.noreply.github.com>
2019-08-03 14:39:35 +0800

    Use remote DNS.

--- examples/transparent-proxy.sh
+++ examples/transparent-proxy.sh
@@ -1,6 +1,13 @@
-iptables -t nat -N V2RAY # 新建一个名为 V2RAY 的链
-iptables -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN # 直连 192.168.0.0/16 
+iptables -t nat -N V2RAY || iptables -t nat -F V2RAY          # 新建或清空 V2RAY 的链
+iptables -t nat -A V2RAY -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
 iptables -t nat -A V2RAY -p tcp -j RETURN -m mark --mark 0xff # 直连 SO_MARK 为 0xff 的流量(0xff 是 16 进制数，数值上等同与上面配置的 255)，此规则目的是避免代理本机(网关)流量出现回环问题
-iptables -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 3455 # 其余流量转发到 12345 端口（即 V2Ray）
-iptables -t nat -A PREROUTING -p tcp -j V2RAY # 对局域网其他设备进行透明代理
-iptables -t nat -A OUTPUT -p tcp -j V2RAY # 对本机进行透明代理
+iptables -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 3455   # 其余流量转发到 3455 端口（即 V2Ray）
+iptables -t nat -A PREROUTING -p tcp -j V2RAY                 # 对局域网其他设备进行透明代理
+# iptables -t nat -A OUTPUT -p tcp -j V2RAY                     # 对本机进行透明代理
